58d4c4ed5bc660db7cfa718f40a3c650db8c6cf1
陈乾 <qianpou@163.com>
2022-06-02 16:40:16 +0800

    添加session超时设置

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 # _*_ codding:utf-8 _*_
+
 from app import create_app
-from flask import render_template
+from flask import render_template, session
+from datetime import timedelta
 from gevent import pywsgi
 
 app = create_app('production')
@@ -12,6 +14,12 @@ def page_not_found(error):
     404
     """
     return render_template("404/404.html"), 404
+
+
+@app.before_request
+def before_request():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=12 * 60)  # session超时 12小时
 
 
 # 正式环境使用
